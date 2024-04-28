18ee69d72977f091de0ea4453fe9c5aea55dc1ed
Woodis <stephane.descarpentries@hotmail.fr>
2024-04-28 23:48:36 +0200

    add responsive orders

--- public/js/orders-index.js
+++ public/js/orders-index.js
@@ -1,14 +1,18 @@
 function toggle(id) {
-    console.log(id);
 
     let divDetail = document.querySelector('.id' + id)
-    if (divDetail.classList.contains('active')) {
-        console.log("true");
-        divDetail.style.height = "0px"
-        divDetail.classList.remove('active')
-    } else {
-        console.log("false");
+    let chevron = document.querySelector(".detail" + id + " img")
+    let order = document.querySelector(".order" + id)
+
+    if (!divDetail.classList.contains('active')) {
+        order.classList.add("order-active")
+        chevron.style.transform = "rotate(180deg)"
         divDetail.classList.add('active')
         divDetail.style.height = divDetail.scrollHeight + "px";
+    } else {
+        order.classList.remove("order-active")
+        chevron.style.transform = "rotate(0deg)"
+        divDetail.style.height = "0px"
+        divDetail.classList.remove('active')
     }
 }
